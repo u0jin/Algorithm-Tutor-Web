a1d457fad6270e2998e01a52bde630609a08bd1c
ujinee <yujin5836@gmail.com>
2020-11-14 07:52:01 +0900

    Update : main.js & submit.ejs
    add testcase , update return json

--- router/main.js
+++ router/main.js
@@ -7,7 +7,7 @@ module.exports = function (app, fs) {
     app.get("/", function (req, res) {  //main page
         res.render('index', {
             length: 3,
-            problem: ["Hello", "good", "world"]
+            problem: ["A+B", "두 수 비교하기", "수 정렬하기"]
         });
         //res.sendFile(path.resolve(__dirname, "index.html"))
     });
@@ -20,22 +20,31 @@ module.exports = function (app, fs) {
     app.get("/submit/:id", function (req, res) {
         problem = [{
             'index': '0',
-            'title': "문자열 내림차순으로 배치하기",
-            'description': "문자열 s에 나타나는 문자를 큰것부터 작은 순으로 정렬해 새로운 문자열을 리턴하는 함수, solution을 완성해주세요. s는 영문 대소문자로만 구성되어 있으며, 대문자는 소문자보다 작은 것으로 간주합니다.",
-            'input': "Zbcdefg",
-            'output': "gfedcbZ",
-            'input_example': "Zbcdefg",
-            "output_example": "gfedcbZ"
+            'title': "A+B",
+            'description': "두 정수 A와 B를 입력받은 다음, A+B를 출력하는 프로그램을 작성하시오.",
+            'input': "첫째 줄에 A와 B가 주어진다. (0 < A, B < 10)",
+            'output': "첫째 줄에 A+B를 출력한다.",
+            'input_example': "1 2",
+            "output_example": "3"
 
         }, {
             'index': '1',
-            'title': "입력하기01",
-            'description': "알아서 풀어야할듯 ㅋㅋ",
-            'input': "3 2",
-            'output': "5",
-            'input_example': "3 2",
-            "output_example": "5"
-        }];
+            'title': "두 수 비교하기",
+            'description': "두 정수 A와 B가 주어졌을 때, A와 B를 비교하는 프로그램을 작성하시오.",
+            'input': "첫째 줄에 A와 B가 주어진다. A와 B는 공백 한 칸으로 구분되어져 있다.",
+            'output': "첫째 줄에 다음 세 가지 중 하나를 출력한다. A가 B보다 큰 경우에는 '>'를 출력한다. A가 B보다 작은 경우에는 '<'를 출력한다. A와 B가 같은 경우에는 '=='를 출력한다.",
+            'input_example': "1 2",
+            "output_example": "<"
+        },{
+         'index': '2',
+         'title': "수 정렬하기",
+         'description': "N개의 수가 주어졌을 때, 이를 오름차순으로 정렬하는 프로그램을 작성하시오.",
+         'input': "처음에 수의 개수 N(1 ≤ N ≤ 1,000)이 주어진다. 둘째 번째부터 N개의 숫자가 주어진다. 이 수는 절댓값이 1,000보다 작거나 같은 정수이다. 수는 중복되지 않는다.",
+         'output': "첫째번째 부터 N개의 수가 오름차순으로 정렬한 결과를 차례로 하나씩 출력한다.",
+         'input_example': "5 5 2 3 4 1",
+         "output_example": "1 2 3 4 5"
+
+     }];
         res.render('submit', {
             index: problem[req.params.id]
         });
@@ -52,6 +61,7 @@ module.exports = function (app, fs) {
         // 컴파일 상태 확인
         var ret = { success: false, stdout: '' };
         var result = "FAIL";
+        var compileResult ="";
         //var src_result ="FAIL";
 
 
@@ -96,16 +106,19 @@ module.exports = function (app, fs) {
             })
         }
 
-        //TODO : 나머지 언어는 추가하면서 수정할것
 
+        compileResult = result;  // compile Result : 컴파일이 잘되었는지 체크 
+
+        //TODO : 나머지 언어는 추가하면서 수정할것
+        var prolist = req.body.index; // problem list = index
 
         if (result != "compile fail") {  // 컴파일을 성공한다면, 정답 체크 ㄱㄱ
             var scorelist = new Array();
             var i = 0;
+            var score = 0; // 점수 합산
+            
 
-            var prolist = req.body.index; // problem list = index
-
-            for (i = 0; i < 2; i++) {  //case list
+            for (i = 0; i < 5; i++) {  //case list
                 // 채점 스크립트
                 const Judge_script = 'sudo ' + `/home/ujin/Desktop/final/Algorithm-Tutor-Web/tester/libjudger.so` + ' ' + '--max_cpu_time=1000' + ' ' +
                     '--max_real_time=2000' + ' ' + '--max_memory=134217728' + ' ' +
@@ -116,13 +129,10 @@ module.exports = function (app, fs) {
 
                 const src_output = fs.readFileSync(`/home/ujin/Desktop/final/Algorithm-Tutor-Web/tester/problem${prolist}/output${i}.txt`, 'utf8').toString();  // 문제의 답 불러옴
 
-                const user_output = fs.readFileSync(`/home/ujin/Desktop/final/Algorithm-Tutor-Web/tester/result_src.txt`, 'utf8').toString();  // user 의 소스 결과값 불러옴        
-
-
-
 
                 try {  // 컴파일 성공
                     const Judgestdout = execSync(Judge_script).toString();
+                    const user_output = fs.readFileSync(`/home/ujin/Desktop/final/Algorithm-Tutor-Web/tester/result_src.txt`, 'utf8').toString();  // user 의 소스 결과값 불러옴        
                     const Judgetatus = JSON.parse(Judgestdout);
                     console.log(Judgetatus);
                     console.log( i + src_output);
@@ -131,6 +141,7 @@ module.exports = function (app, fs) {
 
                     if (user_output == src_output) {
                         result = "성공";
+                        score += 20; //테스트케이스 하나당 20점 >> 5개 다 맞으면 100점
                         //scorelist.push( "통과");
 
                     } else if (user_output != src_output) {
@@ -153,14 +164,14 @@ module.exports = function (app, fs) {
 
 
         }
-        //res.render('submit_src', { result: result, method: "post" });
 
-        var score = result;
+
         //TODO : NO compiler > testcase
 
         console.log(req.body);
         return res.json({
             'value': true,
+            'result': compileResult,
             'score': score,
             'case': scorelist
         });
